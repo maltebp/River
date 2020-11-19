b6e0f43fccea7d41b34469e3c64bc2641c9c1d34
Malte B.P <maltebp94@gmail.com>
2020-11-19 10:30:41 +0100

    Cleaned up AssetCollection

--- src/River/Asset/AssetCollection.cpp
+++ src/River/Asset/AssetCollection.cpp
@@ -2,14 +2,28 @@
 
 namespace River {
 
+	void AssetCollection::add(Asset* asset) {
+		auto iterator = std::find(assets.begin(), assets.end(), asset);
+		if( iterator == assets.end() )
+			assets.push_back(asset);
+	}
+
 	void AssetCollection::load() {
+		if( loaded ) return; // TODO: Add warning here
 		for( auto asset : assets )
 			asset->load();
+		loaded = true;
 	}
 
 	void AssetCollection::unload() {
+		if( !loaded ) return; // TODO: Add warning here
 		for( auto asset : assets )
 			asset->unload();
+		loaded = false;
+	}
+
+	bool AssetCollection::isLoaded() {
+		return loaded;
 	}
 
 }
--- src/River/Asset/AssetCollection.h
+++ src/River/Asset/AssetCollection.h
@@ -15,24 +15,34 @@ namespace River {
 	class AssetCollection {
 	public:
 
-		void load();
-
-		void unload();
-
 		/**
 		 * @brief Adds an asset to this collection
 		 *
 		 * @param a		The asset to add. Does nothing if the asset has already been added
 		*/
-		void add(Asset* a) {
-			auto iterator = std::find(assets.begin(), assets.end(), a);
-			if( iterator == assets.end() )
-				assets.push_back(a);
-		}
+		void add(Asset* a);
 
+		/**
+		 * @brief	Loads all the Assets in this collection. While some of the Assets may already be
+		 *			loaded, this ensures they stay loaded as long as this collection is loaded.
+		 *			Logs a warning if the collection is not already loaded.
+		*/
+		void load();
 
+		/**
+		 * @brief	Unloads all the Assets in this collection. Some Assets may remain loaded, if they
+		 *			have been loaded by any other collection.
+		 *			Logs a warning if the collection is not already loaded.
+		*/
+		void unload();
+
+		/**
+		 * @return	Whether or not this asset is loaded
+		*/
+		bool isLoaded();
 
 	private:
+		bool loaded = false;
 		std::vector<Asset*> assets;
 	};
 }
