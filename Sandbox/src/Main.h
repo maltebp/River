e582a0890c8ed504797fd7b90e6f3a94fc31c3a3
Malte B.P <maltebp94@gmail.com>
2021-08-18 11:15:17 +0200

    Sandbox: Added editor layer for testing

--- Sandbox/src/Main.h
+++ Sandbox/src/Main.h
@@ -6,26 +6,36 @@
 
 
 #include "Assets.h"
+#include "EditorLayer.h"
 #include "MainLayer.h"
 
 
 using River::Game;
 using River::Window;
 
+#define EDITOR_MODE
+
 
 int main() {
 
 	Window::setTitle("Sandbox Game");
 	Window::setResolution({ 1280, 720 });
-
-	// Window::disableMainViewport();
-	// Game::enableImGui();
+	
+	#ifdef EDITOR_MODE
+		Window::disableMainViewport();
+		Game::enableImGui();
+	#endif
 
 	Game::start([]() {
 		GlobalAssets::COLLECTION->load();
 		River::SpriteRenderingSystem::initialize();
 		River::TextRenderingSystem::initialize();	
-		Game::pushLayer<MainLayer>(Window::getMainViewport());
+
+		#ifdef EDITOR_MODE
+			Game::pushLayer<EditorLayer>();
+		#else
+			Game::pushLayer<MainLayer>(Window::getMainViewport());
+		#endif
 	});
 
 	std::cout << "Stopping Sandbox" << std::endl;
