db9aa03f22163da9259a9143b159ab71e52d74cb
Malte Pedersen <maltebp94@gmail.com>
2022-08-22 18:43:33 +0200

    Updated RiverECS version

--- lib/RiverECS/include/ECS/Component.h
+++ lib/RiverECS/include/ECS/Component.h
@@ -27,9 +27,14 @@ namespace River::ECS {
 	*/
 	struct Component {
 	public:
-		ComponentId getId() { return id; }
+
+		ComponentId getId() { 
+			return id;
+		}
+		
 
 	private:
+		
 		/**
 		 * @brief	Unique ID for each component in the same type
 		*/
@@ -40,13 +45,11 @@ namespace River::ECS {
 	};
 
 
-
 	/** Asserts whether or not the given type is a valid component type */
 	#define RV_ECS_ASSERT_COMPONENT_TYPE(x) \
 		static_assert(std::is_base_of<Component, x>::value, "Component type must inherit from ECS::Component");\
 		static_assert(std::is_default_constructible<x>::value, "Component type must have a default constructor")
 				
-
 
 	using ComponentTypeId = int32_t;
 }
