9e0b44f5872bd2071e0a79bc7f67d561d11c0004
Malte Pedersen <maltebp94@gmail.com>
2022-10-16 17:24:20 +0200

    Model rendering: cleaned up a bit

--- src/River/River/Graphics/ModelRenderer.cpp
+++ src/River/River/Graphics/ModelRenderer.cpp
@@ -12,7 +12,7 @@ static std::string vertexShaderSource = R"(
 
     uniform mat4 u_CameraMatrix;
     uniform mat4 u_ModelMatrix;
-    // uniform vec3 u_Albedo;
+    uniform vec3 u_Albedo;
 
     uniform vec3 u_DirectionalDirection;
     uniform vec3 u_DirectionalColor;
@@ -32,7 +32,9 @@ static std::string vertexShaderSource = R"(
             0.5 * a_Normal.z + 0.5
         );
 
-        o_Albedo = normalColor;
+        // o_Albedo = normalColor;
+        o_Albedo = u_Albedo;
+
         vec4 worldPosition = u_ModelMatrix * vec4(a_Position, 1.0);
         o_WorldPosition = worldPosition.xyz;
 
@@ -49,6 +51,8 @@ static std::string fragmentShaderSource = R"(
 
     uniform float u_Gamma;
     uniform vec3 u_DirectionalDirection;
+    uniform vec3 u_DirectionalColor;
+
     uniform vec3 u_AmbientColor;
 
     out vec4 FragColor;
@@ -57,13 +61,19 @@ static std::string fragmentShaderSource = R"(
     in vec3 o_WorldPosition;
     in vec3 o_WorldNormal;
 
+    #define PI 3.1415926538
+
     void main() {
         
-        vec3 directionalRadiance = vec3(1.0) * max(dot(o_WorldNormal, -u_DirectionalDirection), 0.0);
+        vec3 brdf = o_Albedo / PI;
 
-        vec3 radiance = o_Albedo * (directionalRadiance + u_AmbientColor);
+        vec3 incomingRadiance = u_DirectionalColor;
+        
+        vec3 ambientRadiance = o_Albedo * u_AmbientColor; 
 
-        vec3 gammaCorrectedColor = pow(radiance, 1.0 / vec3(u_Gamma));
+        vec3 radiance = ambientRadiance + brdf * incomingRadiance * max(dot(o_WorldNormal, -u_DirectionalDirection), 0.0);
+
+        vec3 gammaCorrectedColor = pow(radiance, vec3(1.0 / u_Gamma));
 
         FragColor = vec4(gammaCorrectedColor, 1.0);
     }
@@ -123,7 +133,7 @@ void ModelRenderer::renderModelInstance(
     shaderProgram.setFloatMatrix("u_CameraMatrix", 4, value_ptr(camera->getMatrix()));
 
     shaderProgram.setFloat3("u_DirectionalDirection", directionalLightDirection);
-    // shaderProgram.setFloat3("u_DirectionalColor", directionalLightColor);
+    shaderProgram.setFloat3("u_DirectionalColor", directionalLightColor);
     // shaderProgram.setFloat3("u_PointPosition", pointLightPosition);
     // shaderProgram.setFloat3("u_PointColor", pointLightColor);
     // shaderProgram.setFloat3("u_DirectionalColor", directionalLightColor);
@@ -136,7 +146,7 @@ void ModelRenderer::renderModelInstance(
         const Material* materialOverride = modelInstance->getMaterialOverride(material);
         const Material* actualMaterial = materialOverride != nullptr ? materialOverride : material;
 
-        // shaderProgram.setFloat3("u_Albedo", actualMaterial->getAlbedo());
+        shaderProgram.setFloat3("u_Albedo", actualMaterial->getAlbedo());
 
         const VertexArray& vertexArray = mesh->getVertexArray();
         vertexArray.bind();
